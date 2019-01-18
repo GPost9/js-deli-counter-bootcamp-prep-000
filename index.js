3ec5e960d341cee66a4c586bd50477cce25dfa86
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-01-18 17:25:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, person){
-  katzDeliLine.push(`${person}`);
+  katzDeliLine.push(person);
   return (`Welcome, ${person}. You are number ${katzDeliLine.length} in line.`);}
 
 function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
+
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   }
   else
-  return (`Currently serving ${katzDeli.shift()}.`);
+    return (`Currently serving ${katzDeli.shift()}.`);
 }
 
 
